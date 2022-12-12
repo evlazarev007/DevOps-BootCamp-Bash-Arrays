db9bc8f885b4842b5b1680b495710468e178087d
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 04:39:36 +0600

    444

--- array.sh
+++ array.sh
@@ -4,6 +4,12 @@ array=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 output="$1 folders created:"
 
 for ((i=0;i<$1;i++)); do
+	mkdir $PWD/folder_${array[i]}
 	output="$output folder_${array[i]},"
 done
-echo "${output/%,/}"
+if [[ $1 -eq 1 ]]; then
+	output=${output/folders/folder}
+	echo "${output/%,/}"
+else
+	echo "${output/%,/}"
+fi
